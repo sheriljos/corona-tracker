25b2655029501e623c24183381ef40f15fc8bbd0
Sheril Elizabeth Jose <sheril@Sherils-MacBook-Pro.local>
2020-11-21 07:56:46 +0100

    Hei we have the initial set up ready

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { div } from 'prelude-ls';
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>Test</div> 
+        )
+    }
+}
+
+export default App;
