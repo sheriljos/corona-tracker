9805d8852413b2fff76183643e988ccb4fc3dc19
Sheril Elizabeth Jose <sheril@Sherils-MacBook-Pro.local>
2020-11-21 08:42:56 +0100

    Let us make all dummy components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import { div } from 'prelude-ls';
 import React from 'react';
+import { Card, Chart, CountryPicker} from './Components';
+import styles from './App.module.css';
 
 class App extends React.Component {
     render() {
         return (
-            <div>Test</div> 
+            <div className={ styles.container }>
+                <Card />
+                <Chart />
+                <CountryPicker />
+            </div> 
         )
     }
 }
